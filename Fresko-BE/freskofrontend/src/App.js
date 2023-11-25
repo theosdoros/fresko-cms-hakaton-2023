ed898a78172c2cc28fec821fce132c46db57e2f1
teodora-velisavljevic <teodora.velisavljevic@pmf.edu.rs>
2023-11-25 23:05:28 +0100

    initial front setup

--- Fresko-BE/freskofrontend/src/App.js
+++ Fresko-BE/freskofrontend/src/App.js
@@ -1,41 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
 import {useEffect, useState} from 'react';
+import {route} from "./vars.js";
 
 
 function App() {
-  const [test, setTest] = useState("");
+  const [user, setUser] = useState(null);
 
-  useEffect( () => {
-  const testfunc = async () => {
-    // await axios.get(`https://localhost:7252/Component/Index`).then(res => {
-    //   console.log(res);
-    //   setTest(res.data);
-    // });
-    // await axios.post(`https://localhost:7252/ArticleText/AddArticle`, {
-    //   text: "asdasdasdas"
-    // }).then(res => {
-    //       console.log(res);
-    //       setTest(res.data);
-    //     })
+  const fetchUser = async () => {
+    await axios.get(route + "/user")
+    .then(res => setUser(res.data))
+    .catch(err => console.log(err))
+  };
 
-    //   await axios.post(`https://localhost:7252/Page/Create`, {
-    //     ParentId: 2,
-    //     PageName: "asdasdasdas",
-    //     CreationDate: "2023-11-25T17:21:51.202Z"
-    // }).then(res => {
-    //       console.log(res.data);
-    //       setTest(res.data);
-    //     });
-  }
-    testfunc();
-  }, [test]);
+  useEffect(() => {
+    //fetchUser;
+  }, [user])
 
   return (
-    <div className="App">
-      <h1>{test}</h1>
-    </div>
+    <BrowserRouter>
+      
+    </BrowserRouter>
   );
 }
 
---
+++ Fresko-BE/freskofrontend/src/pages/backoffice/AdminPanel.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import {route} from "./../../vars.js";
+import {useEffect, useState} from 'react';
+
+export default function AdminPanel() {
+
+    useEffect(() => {
+
+    }, [])
+
+    return(
+        <>
+
+        </>
+    );
+}

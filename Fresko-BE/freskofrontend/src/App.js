00900fce70f050a33c21dc345e5c8aa49a366c42
teodora-velisavljevic <teodora.velisavljevic@pmf.edu.rs>
2023-11-26 00:24:02 +0100

    routing

--- Fresko-BE/freskofrontend/src/App.js
+++ Fresko-BE/freskofrontend/src/App.js
@@ -1,8 +1,17 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
 import {useEffect, useState} from 'react';
 import {route} from "./vars.js";
+
+
+import Login from './pages/auth/Login.js';
+import Register from './pages/auth/Register.js';
+import WebPage from './pages/web/WebPage.js';
+import LandingPage from './pages/web/LandingPage.js';
+import Media from './pages/backoffice/Media.js';
+import AdminPanel from './pages/backoffice/AdminPanel.js';
+import Dashboard from './pages/backoffice/Dashboard.js'
 
 
 function App() {
@@ -20,7 +29,40 @@ function App() {
 
   return (
     <BrowserRouter>
+      <Route 
+        path="/fresko/login"
+        element={<Login/>}
+      />
+      <Route 
+        path="/fresko/register"
+        element={<Register/>}
+      />
+      {
+        user == null &&
+        <>
+          <Route 
+          path="/fresko/dashboard"
+          element={<Dashboard/>}
+        />
+        <Route 
+          path="/fresko/media"
+          element={<Media/>}
+        />
+        <Route 
+          path="/fresko/admin-panel"
+          element={<AdminPanel/>}
+        />
+      </>
+      }
       
+      <Route 
+        path="/fresko/pending"
+        element={<LandingPage/>}
+      />
+      <Route 
+        path="/:pagename"
+        element={<WebPage/>}
+      />
     </BrowserRouter>
   );
 }

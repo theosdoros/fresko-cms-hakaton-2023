b939fa2edbb5d566a5bf03533f5734a3f65fd238
teodora-velisavljevic <teodora.velisavljevic@pmf.edu.rs>
2023-11-26 04:22:04 +0100

    in progress

--- Fresko-BE/freskofrontend/src/pages/web/WebPage.js
+++ Fresko-BE/freskofrontend/src/pages/web/WebPage.js
@@ -1,16 +1,19 @@
-import axios from 'axios';
-import {route} from "./../../vars.js";
-import {useEffect, useState} from 'react';
+import axios from "axios";
+import { route } from "./../../vars.js";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 export default function WebPage() {
+  const [components, setComponents] = useState([]);
+  let params = useParams();
 
-    useEffect(() => {
+  const getComponents = async () => {
+    await axios.get(route + "/");
+  };
 
-    }, [])
+  useEffect(() => {
+    console.log("pageid: ", params.pagename);
+  }, [params.pagename]);
 
-    return(
-        <>
-
-        </>
-    );
+  return <></>;
 }

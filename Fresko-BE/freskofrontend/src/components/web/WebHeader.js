08b2bc6338f8b422c03bd43cc4cad11e1c2818e7
pristbr <pristbrat@gmail.com>
2023-11-26 16:01:33 +0100

    Singup Done

--- Fresko-BE/freskofrontend/src/components/web/WebHeader.js
+++ Fresko-BE/freskofrontend/src/components/web/WebHeader.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 import {route} from "../../vars.js";
 import {useEffect, useState} from 'react';
 
+import { Link, Outlet } from 'react-router-dom';
+
+import Logo from '../../images/LandingPageLogo.svg';
+
 export default function WebHeader() {
 
     useEffect(() => {
@@ -10,7 +14,24 @@ export default function WebHeader() {
 
     return(
         <>
-
+            <div className='homepage-navigation-container'>
+                <div className='navigation-image-container'>
+                    <img src={ Logo } height="100px"/>
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/"> Почетна </Link>
+                        </li>
+                        <li>
+                            <Link to="/fresko/about"> О нама </Link>
+                        </li>
+                        <li>
+                            <Link to="/fresko/login" className='navigation-hero-login'> Пријави се </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </>
     );
 }
